9e750d60a06d752a27b9b7a232a36a22832f9b6b
andronekomimi <lemacon.audrey@gmail.com>
2014-07-27 23:00:09 +0200

    mise en place de la gestion des projets soumis : resupération ok. restitution d'un projet n particulier : not ok. recuperation de l'ensemble des projets : ok!

---
+++ BLASRestlet/src/com/belikeastamp/restlet/ProjectRestletServlet.java
@@ -0,0 +1,114 @@
+package com.belikeastamp.restlet;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.belikeastamp.restlet.model.Project;
+import com.google.gson.Gson;
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cmd.Query;
+
+public class ProjectRestletServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4543801344958314134L;
+
+	private final static Logger LOGGER = Logger.getLogger(ProjectRestletServlet.class.getName()); 
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		LOGGER.setLevel(Level.INFO);
+		ObjectifyService.register(Project.class);
+		Objectify ofy = ObjectifyService.begin();
+		//Query query 
+		String status = req.getParameter("status");
+		String name = req.getParameter("name");
+		String userId = req.getParameter("userid");
+		
+		List<Project> l;
+		Query<Project> query = ofy.load().type(Project.class);
+		
+		if (name != null) {
+			query = query.filter("name", name);
+		}
+		
+		if (status != null)
+			query = query.filter("status", status);
+		
+		if (userId != null)
+			query = query.filter("userId", userId);
+		
+		l = query.list();			
+		
+		
+		LOGGER.info("Taille de l ='"+l.size()+"'");
+		Gson gson = new Gson();
+		String json = gson.toJson(l);
+		System.out.println(json);
+
+		resp.setHeader("User-Agent", "My Custom Header");
+		resp.getWriter().write(json);
+		
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		Long userId = Long.valueOf(req.getParameter("userid"));
+		String name = req.getParameter("name");
+		String subDate = req.getParameter("subdate");
+		String theme = req.getParameter("theme");
+		String type = req.getParameter("type");
+		String orderDate = req.getParameter("orderdate");
+		String perso = req.getParameter("perso");
+		Integer status = Integer.valueOf(req.getParameter("status"));
+		Integer quantity = Integer.valueOf(req.getParameter("quantity"));
+		
+		Project p = new Project(userId, name, subDate, status, theme, type, orderDate, quantity, perso);
+
+		ObjectifyService.register(Project.class);
+		Objectify ofy = ObjectifyService.begin();
+		// Enregistrement de l'objet dans le Datastore avec Objectify
+		ofy.save().entity(p).now();
+	}
+
+	public void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Long id = Long.valueOf(req.getParameter("id"));
+		Long userId = Long.valueOf(req.getParameter("userid"));
+		String name = req.getParameter("name");
+		String subDate = req.getParameter("subdate");
+		String theme = req.getParameter("theme");
+		String type = req.getParameter("type");
+		String orderDate = req.getParameter("orderdate");
+		String perso = req.getParameter("perso");
+		Integer status = Integer.valueOf(req.getParameter("status"));
+		Integer quantity = Integer.valueOf(req.getParameter("quantity"));
+		
+		Project p = new Project(userId, name, subDate, status, theme, type, orderDate, quantity, perso);
+
+		p.setId(id);
+		
+		ObjectifyService.register(Project.class);
+		Objectify ofy = ObjectifyService.begin();
+		ofy.save().entity(p).now();
+	}
+
+	public void doDelete(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		Long id = Long.valueOf(req.getParameter("id"));
+		ObjectifyService.register(Project.class);
+		Objectify ofy = ObjectifyService.begin();
+		ofy.delete().type(Project.class).id(id).now();
+	}
+
+}
---
+++ BLASRestlet/src/com/belikeastamp/restlet/model/Project.java
@@ -0,0 +1,159 @@
+package com.belikeastamp.restlet.model;
+
+import java.io.Serializable;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+@Entity
+public class Project implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8823990281239852140L;
+	
+	@Id private Long id;
+	@Index private Long userId;
+	@Index private String name;
+	private String subDate;
+	private String theme;
+	private String type;
+	private String orderDate;
+	private String perso;
+	@Index private int status;
+	private int quantity;
+
+	public Project() {}
+
+
+
+	public Project(Long userId, String project_name, String sub_date, int project_status,
+			String theme, String type, String order_date, int nbr_cards, String perso) {
+		super();
+		this.userId = userId;
+		this.name = project_name;
+		this.subDate = sub_date;
+		this.status = project_status;
+		this.theme = theme;
+		this.type = type;
+		this.orderDate = order_date;
+		this.quantity = nbr_cards;
+		this.perso = perso;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getSubDate() {
+		return subDate;
+	}
+
+
+
+	public void setSubDate(String subDate) {
+		this.subDate = subDate;
+	}
+
+
+
+	public String getTheme() {
+		return theme;
+	}
+
+
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public String getOrderDate() {
+		return orderDate;
+	}
+
+
+
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+
+
+
+	public int getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+
+	public String getPerso() {
+		return perso;
+	}
+
+
+
+	public void setPerso(String perso) {
+		this.perso = perso;
+	}
+	
+	
+}
